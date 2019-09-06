639ddbb4a638cc8e20292436ae9558d56cd2094b
labkoding <labkoding.id@gmail.com>
2019-09-06 06:11:52 +0100

    pengahpusan
    Silahkan Di hapus Admin ,

--- commons/page_end.php
+++
@@ -1,5 +0,0 @@
-
-<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-</main>
-<?php $this->load->view("$folder_themes/includes/inc_footer.php"); ?>
-</div>
--- includes/inc_marquee.php
+++
@@ -1,8 +0,0 @@
-<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<div class="btn btn-hero btn-rounded btn-danger">
-    <?php foreach ($teks_berjalan AS $teks): ?>
-    <span><?= $teks['teks']?></span>
-        <a href="<?= site_url('first/artikel/'.$teks['tautan']) ?>"><?= $teks['judul_tautan']?> <strong><?php if ($teks['tautan']): ?></strong></a>
-        <?php endif; ?>
-    <?php endforeach; ?>
-</div>
